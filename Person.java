883ad27835918234a199030f4ea67fe1e114d375
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 18:51:54 +0200

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,57 @@
+
+public class Person {
+	private int cID;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String addressLine1;
+	private int socialSecurityNumber;
+
+	public int getcID() {
+		return cID;
+	}
+
+	public void setcID(int cID) {
+		this.cID = cID;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public int getSocialSecurityNumber() {
+		return socialSecurityNumber;
+	}
+
+	public void setSocialSecurityNumber(int socialSecurityNumber) {
+		this.socialSecurityNumber = socialSecurityNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
